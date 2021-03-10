07975c5c30c9e5d3f7712e8ba9cc8c4bf0b9da05
shredder67 <seriy.karp2@gmail.com>
2021-03-10 18:58:29 +0300

    added function headers and modified data

--- pr2.py
+++ pr2.py
@@ -1,1 +1,26 @@
-print("Hello world!")
+import json
+import matplotlib # для визуализации графа
+
+#построение таблицы с оценками критериев
+def build_rate_table(table):
+    pass
+
+def build_ratio_matrix(table):
+    pass
+
+def build_graph(table):
+    pass
+
+#сравнение двух альтернатив
+def compare_alt(alt1, alt2, table):
+    pass
+
+#расчет индексов согласия для ЭЛЕКТРА-II
+def calc_index(P_dominant, P_eq, P_sub):
+    pass
+
+
+with open('pr2_data.json', encoding='utf-8') as json_file:
+    content = json.load(json_file)
+    data = content["data"] 
+    tendencies = content["comp_markers"] #направления воозрастаний критериев
